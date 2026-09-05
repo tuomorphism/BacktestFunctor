1170e5a2bd40fd5e5c701b3b5ab78b6fd51ff893
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2026-09-05 13:04:14 +0300

    parser finished

--- src/Backtest/Parser.hs
+++ src/Backtest/Parser.hs
@@ -1,16 +1,36 @@
-module Backtest.Parser (loadBarData) where
-import Data.Time (Day)
+module Backtest.Parser where
+import Data.Time (Day, parseTimeM, defaultTimeLocale)
+import Data.List (dropWhileEnd)
+import Data.Char ( isSpace ) 
 
-data RawBar = RawBar { rDate :: Day, rOpen :: Double, rHigh :: Double, rLow :: Double, rClose :: Double, rVolume :: Int }
+data RawBar = RawBar { rDate :: Day, rOpen :: Double, rHigh :: Double, rLow :: Double, rClose :: Double, rVolume :: Int } deriving(Show, Eq)
 
+splitOn :: String -> Char -> [String]
+splitOn s c = case splitted of 
+  (part, [])      -> [part]
+  (part, _:rest)  -> part : splitOn rest c
+  where splitted = break (\t -> t == c) s
 
-instance FromNamedRecord RawBar where
-  parseNamedRecord m = RawBar
-    <$> m .: "Date" <*> m .: "Open"  <*> m .: "High"
-    <*> m .: "Low"  <*> m .: "Close" <*> m .: "Volume"
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
 
-instance FromField Day where
-  parseField = maybe (fail "bad date") pure
-             . parseTimeM True defaultTimeLocale "%Y-%m-%d"
-             . B8.unpack
+field :: Read a => String -> String -> Either String a
+field label str  = case reads (trim str) of
+  [(x, "")] -> Right x
+  _         -> Left ("invalid label " ++ label ++ ": " ++ show str)
+
+day :: String -> Either String Day
+day s = case parseTimeM True defaultTimeLocale "%Y-%m-%d" (trim s) of
+  Just d -> Right d
+  Nothing -> Left ("invalid date" ++ show s)
+
+parseLine :: String -> Either String RawBar
+parseLine line = case splitOn line ',' of
+  [d, o, h, l, c, v] ->
+    RawBar  <$> day d
+            <*> field "open"    o
+            <*> field "high"    h
+            <*> field "low"     l
+            <*> field "close"   c
+            <*> field "volume"  v
 
